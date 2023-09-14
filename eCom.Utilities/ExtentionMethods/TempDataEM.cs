ab7b34b27e288282af99ef691ad08eea87321250
ahmedmah010 <reahmedmah010@gmail.com>
2023-09-14 18:44:09 +0300

    Some filteration bug fixes + TempData Extention Method

---
+++ eCom.Utilities/ExtentionMethods/TempDataEM.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ObjectiveC;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCom.Utilities.ExtentionMethods
+{
+    public static class TempDataEM
+    {
+        public static void Push<T>(this ITempDataDictionary tempData, string Key, T Value) where T : class
+        {
+            tempData[Key] = JsonConvert.SerializeObject(Value);
+        }
+        public static T Get<T>(this ITempDataDictionary tempData, string Key) where T : class
+        {
+            object obj;
+            tempData.TryGetValue(Key, out obj);
+            return obj==null?null:JsonConvert.DeserializeObject<T>((string)obj);
+        }
+    }
+}
--- eComApp/Areas/Customer/Controllers/ProductController.cs
+++ eComApp/Areas/Customer/Controllers/ProductController.cs
@@ -20,6 +20,7 @@ namespace eComApp.Areas.Customer.Controllers
         }
         public IActionResult Index()
         {
+            TempData["FilteredProducts"] = null;
             CustomerProductsVM CustomerProductsVM = new CustomerProductsVM();
             CustomerProductsVM.Categories = _CategoryRepo.GetAll().ToList();
             CustomerProductsVM.ProductFilterVM.Brands = new List<BrandCheckBox> { 
@@ -27,16 +28,29 @@ namespace eComApp.Areas.Customer.Controllers
                 new BrandCheckBox {Value="Samsung",IsChecked=false},
             };
             CustomerProductsVM.Products = _ProductRepo.GetAll().ToList();
+            CustomerProductsVM.Products = CustomerProductsVM.Products.Skip(0).Take(PageSize).ToList();
             CustomerProductsVM.PaginationButtons = (int)Math.Ceiling((double)_ProductRepo.Count()/ (double)PageSize);
             return View(CustomerProductsVM);
         }
-        public IActionResult PaginatePV()
-        {
-            return PartialView("~/Views/Shared/Customer_ProductPV/_ProductPV.cshtml",_ProductRepo.GetAll().ToList());
-        }
+       
         public IActionResult Paginate(int PageNumber)
         {
-            return RedirectToAction("Index", new {PageNumber=PageNumber});
+            CustomerProductsVM CustomerProductsVM = new CustomerProductsVM();
+            CustomerProductsVM.Categories = _CategoryRepo.GetAll().ToList();
+            CustomerProductsVM.ProductFilterVM.Brands = new List<BrandCheckBox> {
+                new BrandCheckBox {Value="Apple",IsChecked=false},
+                new BrandCheckBox {Value="Samsung",IsChecked=false},
+            };
+            if (TempData["FilteredProducts"]!=null)
+            {
+                CustomerProductsVM.Products = (List<Product>)TempData.Peek("FilteredProducts");
+            }
+            else
+            {
+                CustomerProductsVM.Products = _ProductRepo.GetAll().ToList();
+            }
+            CustomerProductsVM.Products = CustomerProductsVM.Products.Skip((PageNumber-1)*PageSize).Take(PageSize).ToList();
+            return View("Index", CustomerProductsVM);
         }
         public IActionResult ProductFilter(ProductFilterVM ProductFilterVM, List<string>Brands)
         {
@@ -48,25 +62,34 @@ namespace eComApp.Areas.Customer.Controllers
                 new BrandCheckBox {Value="Apple",IsChecked=false},
                 new BrandCheckBox {Value="Samsung",IsChecked=false},
             };
-            bool IsFiltered = false;
+            bool IsFiltered = false; //Helps to handle the case if a user hardcoded filters in the URL
             if (!Brands.IsNullOrEmpty())
             {
                 IsFiltered = true;
-                CustomerProductsVM.Products = CustomerProductsVM.Products.Where(p=>Brands.Contains(p.Brand)).ToList();
-                foreach(var CheckBoxBrand in ProductFilterVM.Brands) //for UI purposes only (Make checkbox checked automatically)
+                CustomerProductsVM.Products = CustomerProductsVM.Products.Where(p => Brands.Contains(p.Brand)).ToList();
+                foreach (var CheckBoxBrand in ProductFilterVM.Brands) //for UI purposes only (Make checkbox checked automatically)
                 {
-                    if(Brands.Contains(CheckBoxBrand.Value))
+                    if (Brands.Contains(CheckBoxBrand.Value))
                     {
                         CheckBoxBrand.IsChecked = true;
                     }
                 }
             }
-            if(!String.IsNullOrEmpty(ProductFilterVM.Category))
+            if (!String.IsNullOrEmpty(ProductFilterVM.Category))
             {
                 IsFiltered = true;
-                CustomerProductsVM.Products = CustomerProductsVM.Products.Where(p=>p.Category.Name==ProductFilterVM.Category).ToList();
+                CustomerProductsVM.Products = CustomerProductsVM.Products.Where(p => p.Category.Name == ProductFilterVM.Category).ToList();
             }
-            return IsFiltered?View("Index", CustomerProductsVM):RedirectToAction("Index");
+            if (IsFiltered)
+            {
+                CustomerProductsVM.PaginationButtons = (int)Math.Ceiling((double)CustomerProductsVM.Products.Count / (double)PageSize);
+                TempData["FilteredProducts"] = CustomerProductsVM.Products;
+                return View("Index", CustomerProductsVM);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
         }
     }
 }
